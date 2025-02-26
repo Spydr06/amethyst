8792559ce68b96eeeaa545c8c52bd7a826e39fb1
Spydr06 <spydr06@web.de>
2025-02-26 23:50:13 +0100

    kernel: add keyboard and mouse interrupt handlers

--- drivers/char/ps2_keyboard.c
+++ drivers/char/ps2_keyboard.c
@@ -2,11 +2,13 @@
 
 #include <x86_64/cpu/idt.h>
 #include <x86_64/dev/apic.h>
+#include <x86_64/dev/io.h>
 
 #include <assert.h>
 #include <kernelio.h>
 
 static void keyboard_isr(struct cpu_context* ctx) {
+    uint8_t scancode = inb(PS2_PORT_DATA);
     here();
 }
 
@@ -14,5 +16,6 @@ void ps2_keyboard_init(void) {
     struct isr* isr = interrupt_allocate(keyboard_isr, apic_send_eoi, IPL_KEYBOARD);
     assert(isr);
     io_apic_register_interrupt(KEYBOARD_INTERRUPT, isr->id & 0xff, _cpu()->id, false);
-    here(); 
+    klog(INFO, "PS/2 keyboard interrupt enabled with vector %lu", isr->id & 0xff);
 }
+
--- drivers/char/ps2_mouse.c
+++ drivers/char/ps2_mouse.c
@@ -1,5 +1,22 @@
+#include "x86_64/cpu/idt.h"
 #include <drivers/char/ps2.h>
 
-void ps2_mouse_init(void) {
+#include <x86_64/cpu/idt.h>
+#include <x86_64/dev/apic.h>
+#include <x86_64/dev/io.h>
 
+#include <assert.h>
+#include <kernelio.h>
+
+static void mouse_isr(struct cpu_context* ctx) {
+    uint8_t data = inb(PS2_PORT_DATA);
+    here();
 }
+
+void ps2_mouse_init(void) {
+    struct isr* isr = interrupt_allocate(mouse_isr, apic_send_eoi, IPL_MOUSE);
+    assert(isr);
+    io_apic_register_interrupt(MOUSE_INTERRUPT, isr->id & 0xff, _cpu()->id, false);
+    klog(INFO, "PS/2 mouse interrupt enabled with vector %lu", isr->id & 0xff);
+}
+
